af128eae2fe5d198f2d17c391de08a0bb47ce5e9
Fancy Cheng <haicheng@cisco.com>
2022-07-28 12:05:04 +0800

    zk

--- docker/mats-confluent/cp-zookeeper-il5/zk_run.sh
+++ docker/mats-confluent/cp-zookeeper-il5/zk_run.sh
@@ -9,20 +9,22 @@ JAVA_OPTS="$JAVA_OPTS \
     -Djavax.net.ssl.trustStoreType=BCFKS \
     -Djavax.net.ssl.keyStoreProvider=BCFIPS \
     -Djavax.net.ssl.trustStoreProvider=BCFIPS \
-    -Djdk.tls.server.protocols=TLSv1.2 \
-    -Dzookeeper.serverCnxnFactory=org.apache.zookeeper.server.NettyServerCnxnFactory \
-    -Dzookeeper.ssl.keyStore.location=/etc/datahub/certs/server.keystore.bcfks \
-    -Dzookeeper.ssl.keyStore.password=$SSL_KEYSTORE_PASS \
-    -Dzookeeper.ssl.keyStore.type=BCFIPS \
-    -Dzookeeper.ssl.trustStore.location=/etc/datahub/certs/server.truststore.bcfks \
-    -Dzookeeper.ssl.trustStore.password=$SSL_TRUSTSTORE_PASS \
-    -Dzookeeper.ssl.trustStore.type=BCFIPS \
-    -Dzookeeper.ssl.quorum.keyStore.location=/etc/datahub/certs/server.keystore.bcfks \
-    -Dzookeeper.ssl.quorum.keyStore.password=$SSL_KEYSTORE_PASS \
-    -Dzookeeper.ssl.quorum.keyStore.type=BCFIPS \
-    -Dzookeeper.ssl.quorum.trustStore.location=/etc/datahub/certs/server.truststore.bcfks \
-    -Dzookeeper.ssl.quorum.trustStore.password=$SSL_TRUSTSTORE_PASS \
-    -Dzookeeper.ssl.quorum.trustStore.type=BCFIPS \
-    -Dzookeeper.ssl.quorum.hostnameVerification=false"
+    -Djdk.tls.server.protocols=TLSv1.2"
 
-. /etc/confluent/docker/run
+ZOOKEEPER_SERVER_CNXN_FACTORY=org.apache.zookeeper.server.NettyServerCnxnFactory
+ZOOKEEPER_SSL_KEYSTORE_LOCATION=/etc/datahub/certs/server.keystore.bcfks
+ZOOKEEPER_SSL_KEYSTORE_PASSWORD=$SSL_KEYSTORE_PASS
+ZOOKEEPER_SSL_KEYSTORE_TYPE=BCFIPS
+ZOOKEEPER_SSL_TRUSTSTORE_LOCATION=/etc/datahub/certs/server.truststore.bcfks
+ZOOKEEPER_SSL_TRUSTSTORE_PASSWORD=$SSL_TRUSTSTORE_PASS
+ZOOKEEPER_SSL_TRUSTSTORE_TYPE=BCFIPS
+ZOOKEEPER_SSL_ENABLED_PROTOCOLS=TLSv1.2
+ZOOKEEPER_SSL_QUORUM_KEYSTORE_LOCATION=/etc/datahub/certs/server.keystore.bcfks
+ZOOKEEPER_SSL_QUORUM_KEYSTORE_PASSWORD=$SSL_KEYSTORE_PASS
+ZOOKEEPER_SSL_QUORUM_KEYSTORE_TYPE=BCFIPS
+ZOOKEEPER_SSL_QUORUM_TRUSTSTORE_LOCATION=/etc/datahub/certs/server.truststore.bcfks
+ZOOKEEPER_SSL_QUORUM_TRUSTSTORE_PASSWORD=$SSL_TRUSTSTORE_PASS
+ZOOKEEPER_SSL_QUORUM_TRUSTSTORE_TYPE=BCFIPS
+ZOOKEEPER_SSL_QUORUM_ENABLED_PROTOCOLS=TLSv1.2
+
+exec /etc/confluent/docker/run
