756b5a8aaf0c22f56c836c3ec657b0332f9be360
Fancy Cheng <haicheng@cisco.com>
2022-07-28 21:29:34 +0800

    ssl

--- docker/mats-confluent/cp-kafka-il5/ka_run.sh
+++ docker/mats-confluent/cp-kafka-il5/ka_run.sh
@@ -13,23 +13,29 @@ export KAFKA_SSL_TRUSTSTORE_CREDENTIALS=truststore.credentials
 
 export KAFKA_DATA_DIRS=/var/kafka/data
 
+SSL_KEYSTORE_LOCATION=/etc/kafka/secrets/server.keystore.bcfks
+SSL_KEYSTORE_PASS=$(cat "/etc/kafka/secrets/keystore.credentials")
+SSL_TRUSTSTORE_LOCATION=/etc/kafka/secrets/server.truststore.bcfks
+SSL_TRUSTSTORE_PASS=$(cat "/etc/kafka/secrets/truststore.credentials")
+
 JAVA_OPTS="$JAVA_OPTS \
-    -Djavax.net.ssl.keyStore=/etc/kafka/secrets/server.keystore.bcfks \
-    -Djavax.net.ssl.keyStorePassword=$(cat "/etc/kafka/secrets/$KAFKA_SSL_KEYSTORE_CREDENTIALS") \
-    -Djavax.net.ssl.trustStore=/etc/kafka/secrets/server.truststore.bcfks \
-    -Djavax.net.ssl.trustStorePassword=$(cat "/etc/kafka/secrets/$KAFKA_SSL_TRUSTSTORE_CREDENTIALS") \
+    -Djavax.net.ssl.keyStore=$SSL_KEYSTORE_LOCATION \
+    -Djavax.net.ssl.keyStorePassword=$SSL_KEYSTORE_PASS \
     -Djavax.net.ssl.keyStoreType=BCFKS \
-    -Djavax.net.ssl.trustStoreType=BCFKS \
     -Djavax.net.ssl.keyStoreProvider=BCFIPS \
+    -Djavax.net.ssl.trustStore=$SSL_TRUSTSTORE_LOCATION \
+    -Djavax.net.ssl.trustStorePassword=$SSL_TRUSTSTORE_PASS \
+    -Djavax.net.ssl.trustStoreType=BCFKS \
     -Djavax.net.ssl.trustStoreProvider=BCFIPS \
-    -Djdk.tls.server.protocols=TLSv1.2 \
-    -Dzookeeper.clientCnxnSocket=org.apache.zookeeper.ClientCnxnSocketNetty \
-    -Dzookeeper.client.secure=true \
-    -Dzookeeper.ssl.keyStore.location=/etc/kafka/secrets/server.keystore.bcfks \
-    -Dzookeeper.ssl.keyStore.password=$(cat "/etc/kafka/secrets/$KAFKA_SSL_KEYSTORE_CREDENTIALS") \
-    -Dzookeeper.ssl.trustStore.location=/etc/kafka/secrets/server.truststore.bcfks \
-    -Dzookeeper.ssl.trustStore.password=$(cat "/etc/kafka/secrets/$KAFKA_SSL_TRUSTSTORE_CREDENTIALS") \
-    -Dzookeeper.ssl.trustStore.type=BCFKS \
-    -Dzookeeper.ssl.keyStore.type=BCFKS"
+    -Djdk.tls.server.protocols=TLSv1.2"
+
+export KAFKA_ZOOKEEPER_CLIENT_CNXN_SOCKET=org.apache.zookeeper.ClientCnxnSocketNetty
+export KAFKA_ZOOKEEPER_client_secure=true
+export KAFKA_ZOOKEEPER_ssl.keyStore.location=$SSL_KEYSTORE_LOCATION
+export KAFKA_ZOOKEEPER_ssl.keyStore.password="$SSL_KEYSTORE_PASS"
+export KAFKA_ZOOKEEPER_ssl.keyStore.type=BCFKS
+export KAFKA_ZOOKEEPER_ssl.trustStore.location=$SSL_TRUSTSTORE_LOCATION
+export KAFKA_ZOOKEEPER_ssl.trustStore.password="$SSL_TRUSTSTORE_PASS"
+export KAFKA_ZOOKEEPER_ssl.trustStore.type=BCFKS
 
 exec /etc/confluent/docker/run
