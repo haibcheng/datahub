67a5d283180ed272634016957f26449a1c651356
Fancy Cheng <haicheng@cisco.com>
2022-07-28 16:06:59 +0800

    certs

--- docker/mats-confluent/cp-kafka-il5/ka_run.sh
+++ docker/mats-confluent/cp-kafka-il5/ka_run.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
+export KAFKA_SSL_KEYSTORE_TYPE=BCFIPS
+export KAFKA_SSL_ENABLED_PROTOCOLS=TLSv1.2
+export KAFKA_SSL_TRUSTSTORE_TYPE=BCFIPS
+export KAFKA_SECURITY_INTER_BROKER_PROTOCOL=TLSv1.2
+
+export KAFKA_SSL_KEY_CREDENTIALS=key.credentials
+export KAFKA_SSL_KEYSTORE_FILENAME=server.keystore.bcfks
+export KAFKA_SSL_KEYSTORE_CREDENTIALS=keystore.credentials
+export KAFKA_SSL_TRUSTSTORE_FILENAME=server.truststore.bcfks
+export KAFKA_SSL_TRUSTSTORE_CREDENTIALS=truststore.credentials
+
 JAVA_OPTS="$JAVA_OPTS \
-    -Djavax.net.ssl.keyStore=/etc/datahub/certs/server.keystore.bcfks \
-    -Djavax.net.ssl.keyStorePassword=$SSL_KEYSTORE_PASS \
-    -Djavax.net.ssl.trustStore=/etc/datahub/certs/server.truststore.bcfks \
-    -Djavax.net.ssl.trustStorePassword=$SSL_TRUSTSTORE_PASS \
+    -Djavax.net.ssl.keyStore=/etc/kafka/secrets/server.keystore.bcfks \
+    -Djavax.net.ssl.keyStorePassword=$(cat "/etc/kafka/secrets/$KAFKA_SSL_KEYSTORE_CREDENTIALS") \
+    -Djavax.net.ssl.trustStore=/etc/kafka/secrets/server.truststore.bcfks \
+    -Djavax.net.ssl.trustStorePassword=$(cat "/etc/kafka/secrets/$KAFKA_SSL_TRUSTSTORE_CREDENTIALS") \
     -Djavax.net.ssl.keyStoreType=BCFKS \
     -Djavax.net.ssl.trustStoreType=BCFKS \
     -Djavax.net.ssl.keyStoreProvider=BCFIPS \
     -Djavax.net.ssl.trustStoreProvider=BCFIPS \
     -Djdk.tls.server.protocols=TLSv1.2"
-
-export KAFKA_SSL_KEY_PASSWORD=$SSL_KEYSTORE_PASS
-export KAFKA_SSL_KEYSTORE_LOCATION=/etc/datahub/certs/server.keystore.bcfks
-export KAFKA_SSL_KEYSTORE_PASSWORD=$SSL_KEYSTORE_PASS
-export KAFKA_SSL_KEYSTORE_TYPE=BCFIPS
-export KAFKA_SSL_ENABLED_PROTOCOLS=TLSv1.2
-export KAFKA_SSL_TRUSTSTORE_LOCATION=/etc/datahub/certs/server.truststore.bcfks
-export KAFKA_SSL_TRUSTSTORE_PASSWORD=$SSL_TRUSTSTORE_PASS
-export KAFKA_SSL_TRUSTSTORE_TYPE=BCFIPS
-export KAFKA_SECURITY_INTER_BROKER_PROTOCOL=TLSv1.2
 
 exec /etc/confluent/docker/run
--- docker/mats-confluent/cp-zookeeper-il5/zk_run.sh
+++ docker/mats-confluent/cp-zookeeper-il5/zk_run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+SSL_KEYSTORE_LOCATION=/etc/zookeeper/secrets/server.keystore.bcfks
+SSL_KEYSTORE_PASS=$(cat "/etc/zookeeper/secrets/keystore.credentials")
+SSL_TRUSTSTORE_LOCATION=/etc/zookeeper/secrets/server.truststore.bcfks
+SSL_TRUSTSTORE_PASS=$(cat "/etc/zookeeper/secrets/truststore.credentials")
+
 JAVA_OPTS="$JAVA_OPTS \
-    -Djavax.net.ssl.keyStore=/etc/datahub/certs/server.keystore.bcfks \
+    -Djavax.net.ssl.keyStore=$SSL_KEYSTORE_LOCATION \
     -Djavax.net.ssl.keyStorePassword=$SSL_KEYSTORE_PASS \
-    -Djavax.net.ssl.trustStore=/etc/datahub/certs/server.truststore.bcfks \
+    -Djavax.net.ssl.trustStore=$SSL_TRUSTSTORE_LOCATION \
     -Djavax.net.ssl.trustStorePassword=$SSL_TRUSTSTORE_PASS \
     -Djavax.net.ssl.keyStoreType=BCFKS \
     -Djavax.net.ssl.trustStoreType=BCFKS \
@@ -12,17 +17,17 @@ JAVA_OPTS="$JAVA_OPTS \
     -Djdk.tls.server.protocols=TLSv1.2"
 
 export ZOOKEEPER_SERVER_CNXN_FACTORY=org.apache.zookeeper.server.NettyServerCnxnFactory
-export ZOOKEEPER_SSL_KEYSTORE_LOCATION=/etc/datahub/certs/server.keystore.bcfks
+export ZOOKEEPER_SSL_KEYSTORE_LOCATION=$SSL_KEYSTORE_LOCATION
 export ZOOKEEPER_SSL_KEYSTORE_PASSWORD=$SSL_KEYSTORE_PASS
 export ZOOKEEPER_SSL_KEYSTORE_TYPE=BCFIPS
-export ZOOKEEPER_SSL_TRUSTSTORE_LOCATION=/etc/datahub/certs/server.truststore.bcfks
+export ZOOKEEPER_SSL_TRUSTSTORE_LOCATION=$SSL_TRUSTSTORE_LOCATION
 export ZOOKEEPER_SSL_TRUSTSTORE_PASSWORD=$SSL_TRUSTSTORE_PASS
 export ZOOKEEPER_SSL_TRUSTSTORE_TYPE=BCFIPS
 export ZOOKEEPER_SSL_ENABLED_PROTOCOLS=TLSv1.2
-export ZOOKEEPER_SSL_QUORUM_KEYSTORE_LOCATION=/etc/datahub/certs/server.keystore.bcfks
+export ZOOKEEPER_SSL_QUORUM_KEYSTORE_LOCATION=$SSL_KEYSTORE_LOCATION
 export ZOOKEEPER_SSL_QUORUM_KEYSTORE_PASSWORD=$SSL_KEYSTORE_PASS
 export ZOOKEEPER_SSL_QUORUM_KEYSTORE_TYPE=BCFIPS
-export ZOOKEEPER_SSL_QUORUM_TRUSTSTORE_LOCATION=/etc/datahub/certs/server.truststore.bcfks
+export ZOOKEEPER_SSL_QUORUM_TRUSTSTORE_LOCATION=$SSL_TRUSTSTORE_LOCATION
 export ZOOKEEPER_SSL_QUORUM_TRUSTSTORE_PASSWORD=$SSL_TRUSTSTORE_PASS
 export ZOOKEEPER_SSL_QUORUM_TRUSTSTORE_TYPE=BCFIPS
 export ZOOKEEPER_SSL_QUORUM_ENABLED_PROTOCOLS=TLSv1.2
