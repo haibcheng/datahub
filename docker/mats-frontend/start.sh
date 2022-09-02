08f78ebde2f0efdbc4b835f083b8e4ebd4b56c71
Fancy Cheng <haicheng@cisco.com>
2022-09-02 17:15:28 +0800

    frontend docker

---
+++ docker/mats-frontend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+JAVA_OPTS="$JAVA_OPTS -Dhttp.port=$SERVER_PORT \
+    -Dplay.server.akka.max-header-value-length=40960 \
+    -Dconfig.file=datahub-frontend/conf/application.conf \
+    -Djava.security.auth.login.config=datahub-frontend/conf/jaas.conf \
+    -Dlogback.configurationFile=datahub-frontend/conf/logback.xml \
+    -Dpidfile.path=/dev/null"
+
+source /datahub-frontend/bin/playBinary
