5a3d0e466b1e35ea15e76dd5a65dc2abfef3dd26
Fancy Cheng <haicheng@cisco.com>
2022-10-16 22:41:26 +0800

    mats zk & gms & frontend docker

---
+++ docker/mats-frontend/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+[ ! -f /etc/datahub/env/frontend.env ] || export $(grep -v '^#' /etc/datahub/env/frontend.env | xargs)
+
+JAVA_OPTS="$JAVA_OPTS -Dhttp.port=$SERVER_PORT \
+    -Dplay.server.akka.max-header-value-length=40960 \
+    -Dconfig.file=datahub-frontend/conf/application.conf \
+    -Djava.security.auth.login.config=datahub-frontend/conf/jaas.conf \
+    -Dlogback.configurationFile=datahub-frontend/conf/logback.xml \
+    -Dpidfile.path=/dev/null"
+
+exec /datahub-frontend/bin/datahub-frontend
