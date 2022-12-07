b436fe2973ee725e2578e08900355c4f906f1ece
haicheng <haicheng@cisco.com>
2022-12-07 10:54:54 +0800

    mats-docker

--- docker/mats-zookeeper/zkRun.sh
+++ docker/mats-zookeeper/zkRun.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+set -x
+
+if [[ -v ZOOKEEPER_SERVERS ]];
+then
+  IFS=';' read -r -a servers <<< "$ZOOKEEPER_SERVERS"
+  for index in "${!servers[@]}"
+  do
+    echo "server.$(($index+1))=${servers[index]}" >> /opt/apache-zookeeper/conf/zoo.cfg
+  done
+fi
+
+if [ ! -f /var/zookeeper/data/myid ]
+then
+  echo "$ZOOKEEPER_SERVER_ID" >> /var/zookeeper/data/myid
+fi
 
 exec /opt/apache-zookeeper/bin/zkServer.sh start-foreground
